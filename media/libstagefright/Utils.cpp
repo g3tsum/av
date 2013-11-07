DolbyLaboratories/caf-platform-frameworks-av

frameworks/av: AAC CSD support for MediaCodec path
- AudioFlush CTS test case is failing for AAC

- AAC Codec specific data is missing for
MediaCodec path for AAC if we use Qualcomm
ExtendedExtractor for demuxing

- Added CSD buffer while converting from
metadata to message , which can be used by
ACodec/MediaCodec

Change-Id: Ic8197138f5107f2a6026c28847e74987d91acc16
CRs-Fixed: 509575

media/libstagefright/Utils.cpp
@@ -28,6 +28,10 @@
 #include <media/stagefright/MetaData.h>
 #include <media/stagefright/Utils.h>
 
+#ifdef ENABLE_QC_AV_ENHANCEMENTS
+#include "QCMetaData.h"
+#endif
+
 namespace android {
 
 uint16_t U16_AT(const uint8_t *ptr) {
@@ -123,6 +127,30 @@ status_t convertMetaDataToMessage(
         if (meta->findInt32(kKeyIsADTS, &isADTS)) {
             msg->setInt32("is-adts", true);
         }
+
+#ifdef ENABLE_QC_AV_ENHANCEMENTS
+        uint32_t type;
+        const void *data;
+        size_t size;
+
+        if (meta->findData(kKeyAacCodecSpecificData, &type, &data, &size)) {
+            if (size > 0 && data != NULL) {
+                sp<ABuffer> buffer = new ABuffer(size);
+                if (buffer != NULL) {
+                    memcpy(buffer->data(), data, size);
+                    buffer->meta()->setInt32("csd", true);
+                    buffer->meta()->setInt64("timeUs", 0);
+                    msg->setBuffer("csd-0", buffer);
+                }
+                else {
+                    ALOGE("kKeyAacCodecSpecificData ABuffer Allocation failed");
+                }
+            }
+            else {
+                ALOGE("Not a valid data pointer or size == 0");
+            }
+       }
+#endif
     }
 
     int32_t maxInputSize;

g3tsum/av

(cherry picked from commit ae1b84bc3ead6a94e1383974e1bcba786b8e9354)
(cherry picked from commit 9e6ee7ae52b3bcb3cc76011d849e14a18f55bce5)
(cherry picked from commit 7edbc7b3a79cc65e355b8aa6025de15930fd0a2a)

media/libstagefright/Utils.cpp
@@ -34,6 +34,10 @@
 #include <media/AudioParameter.h>
 #include <media/stagefright/ExtendedCodec.h>
 
+#ifdef ENABLE_AV_ENHANCEMENTS
+#include "QCMetaData.h"
+#endif
+
 namespace android {
 
 uint16_t U16_AT(const uint8_t *ptr) {
@@ -129,6 +133,30 @@ status_t convertMetaDataToMessage(
         if (meta->findInt32(kKeyIsADTS, &isADTS)) {
             msg->setInt32("is-adts", true);
         }
+
+#ifdef ENABLE_AV_ENHANCEMENTS
+        uint32_t type;
+        const void *data;
+        size_t size;
+
+        if (meta->findData(kKeyAacCodecSpecificData, &type, &data, &size)) {
+            if (size > 0 && data != NULL) {
+                sp<ABuffer> buffer = new ABuffer(size);
+                if (buffer != NULL) {
+                    memcpy(buffer->data(), data, size);
+                    buffer->meta()->setInt32("csd", true);
+                    buffer->meta()->setInt64("timeUs", 0);
+                    msg->setBuffer("csd-0", buffer);
+                }
+                else {
+                    ALOGE("kKeyAacCodecSpecificData ABuffer Allocation failed");
+                }
+            }
+            else {
+                ALOGE("Not a valid data pointer or size == 0");
+            }
+       }
+#endif
     }
 
     int32_t maxInputSize;